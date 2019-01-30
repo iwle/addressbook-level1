37020072ec057d428fd92971d4159be194867f89
Ian Wu <iwle@u.nus.edu>
2019-01-30 14:18:20 +0800

    Demonstration of commit.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -42,6 +42,7 @@ public class AddressBook {
      * Default file path used if the user doesn't provide the file name.
      */
     private static final String DEFAULT_STORAGE_FILEPATH = "addressbook.txt";
+    private static final String HELLO_WORLD = "hello world"; // demonstration
 
     /**
      * Version info of the program.
